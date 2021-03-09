c532d83cf35e825e05fd2585f8346cf18ec25eb2
Mushy-Avocado <79934091+Mushy-Avocado@users.noreply.github.com>
2021-03-09 13:52:35 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
+var gameState = {}
+
 function preload () {
- 
+ this.image.load('logo', 'https://github.com/Mushy-Avocado/testing/blob/main/github%20image.png')
 }
 
 function create () {
-  
+  gameState.logo = this.sprite.add(50, 50, 'logo')
 }
 
 const config = {
