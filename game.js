9854baa780074c9d583d8bc9de058e327378cbef
Mushy-Avocado <79934091+Mushy-Avocado@users.noreply.github.com>
2021-03-11 15:42:05 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,21 +1,21 @@
-var gameState = {}
+var programCode = function(processingInstance) {
+    with (processingInstance) {
+      size(400, 400); 
+      frameRate(30);
+        
+      // Paste code from Khan Academy here:
+      fill(255, 255, 0);
+      ellipse(200, 200, 200, 200);
+      noFill();
+      stroke(0, 0, 0);
+      strokeWeight(2);
+      arc(200, 200, 150, 100, 0, PI);
+      fill(0, 0, 0);
+      ellipse(250, 200, 10, 10);
+      ellipse(153, 200, 10, 10);
+    }};
 
-function preload () {
- this.image.load('logo', 'https://github.com/Mushy-Avocado/testing/blob/main/github%20image.png')
-}
-
-function create () {
-  gameState.logo = this.sprite.add(50, 50, 'logo')
-}
-
-const config = {
-  width: 400,
-  height: 400,
-  backgroundColor: 0x2aa1d4,
-  scene: {
-    preload,
-    create,
-  }
-}
-
-const game = new Phaser.Game(config)
+  // Get the canvas that ProcessingJS will use
+  var canvas = document.getElementById("mycanvas"); 
+  // Pass the function to ProcessingJS constructor
+  var processingInstance = new Processing(canvas, programCode); 
